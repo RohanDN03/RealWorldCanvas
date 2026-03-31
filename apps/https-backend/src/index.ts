43216e6ba48726dbefbf712f480b9e7ece95bbe1
RohanDN03 <rohanikam63@gmail.com>
2026-03-31 22:42:02 +0530

    PORT environment variable

--- apps/https-backend/src/index.ts
+++ apps/https-backend/src/index.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors())
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 10000;
 
 // Health check endpoint
 app.get("/health", (req, res) => {
